3368cd79ecd9728be4a222c332af91d87eae3aaf
ZeRo <109825197+ZeRo-RX@users.noreply.github.com>
2024-01-26 17:04:31 +0330

    random-pin

--- random-pin/random-pin-4pin.py
+++ random-pin/random-pin-4pin.py
@@ -2,11 +2,11 @@ import time
 import machine
 import random
 
-pin_numbers = [2, 4, 16, 17, 5, 18, 19, 21]
+pin_numbers = [2, 4, 18, 19]
 pins = [machine.Pin(pin, machine.Pin.OUT) for pin in pin_numbers]
 
 while True:
-    x = random.choice(pins)# Write your code here :-)
+    x = random.choice(pins)
     pinx = x
     pinx.on()
     time.sleep(0.1)
