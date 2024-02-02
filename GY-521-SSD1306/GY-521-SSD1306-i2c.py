7be9942b4df86be2f8ba63d893573b0f17d7bc7d
ZeRo <109825197+ZeRo-RX@users.noreply.github.com>
2024-02-02 20:47:26 +0330

    accel_gyro

--- GY-521-SSD1306/GY-521-SSD1306-i2c.py
+++ GY-521-SSD1306/GY-521-SSD1306-i2c.py
@@ -2,27 +2,31 @@ from machine import I2C, Pin
 import mpu6050
 import OLED
 import time
+import math
 
 i2c = I2C(scl=Pin(22), sda=Pin(21))
 mpu = mpu6050.accel(i2c)
 oled = OLED.SSD1306_I2C(128, 64, i2c)
 
+gyro_scale_factor = 1 / 131.072  # بر اساس دیتا‌شیت MPU6050
+accel_scale_factor = 1 / 2.0  # تقسیم بر ۲ برای شتاب سنج
+
 while True:
     mpu_data = mpu.get_values()
 
-    gyro_x = mpu_data['GyX']
-    gyro_y = mpu_data['GyY']
-    gyro_z = mpu_data['GyZ']
+    gyro_x = mpu_data['GyX'] * gyro_scale_factor
+    gyro_y = mpu_data['GyY'] * gyro_scale_factor
+    gyro_z = mpu_data['GyZ'] * gyro_scale_factor
 
-    accel_x = mpu_data['AcX']
-    accel_y = mpu_data['AcY']
-    accel_z = mpu_data['AcZ']
+    accel_x = mpu_data['AcX'] * accel_scale_factor
+    accel_y = mpu_data['AcY'] * accel_scale_factor
+    accel_z = mpu_data['AcZ'] * accel_scale_factor
 
     temperature = mpu_data['Tmp']
 
-    gx = "GX{}".format(gyro_x)
-    gy = "GY{}".format(gyro_y)
-    gz = "GZ{}".format(gyro_z)
+    gx = "GX{:.2f}".format(gyro_x)
+    gy = "GY{:.2f}".format(gyro_y)
+    gz = "GZ{:.2f}".format(gyro_z)
 
     ax = "AX{}".format(accel_x)
     ay = "AY{}".format(accel_y)
