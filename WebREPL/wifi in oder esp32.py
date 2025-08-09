cdc0d45e18945ad17213bf0d9f4c46c577e1db0b
ZeRo <109825197+ZeRo-RX@users.noreply.github.com>
2025-08-09 14:42:38 +0330

    webrepl edit

--- WebREPL/wifi in oder esp32.py
+++ WebREPL/wifi in oder esp32.py
@@ -1,8 +1,6 @@
 import network
-essid='ESP32_AP'
-password='12345678'
-
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid, password)
+ap.config(essid='ESP32_AP')
+ap.config(password='')
 print('AP IP:', ap.ifconfig()[0])
